cd5e106ae52e1fbe2499c8480949b471c765a79f
Shinesa Chowdhury <shinesac@yahoo.com>
2020-09-04 20:15:57 -0400

    finish adjustments

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ function onClickMenu(){
 	document.getElementById("navigation").classList.toggle("change");
 }
 
+//Get the button:
+topbutton = document.getElementById("arrow");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
 
